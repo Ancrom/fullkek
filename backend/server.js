aed0bf7902a5d65f8966b73540cf496ebc3cd88b
r.antsupov <rremon2537@gmail.com>
2025-12-08 15:42:28 +0300

    issue #6: Реализовать базовый HTTP-сервер

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
