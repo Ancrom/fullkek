c6fa8692223d0f603ccf429f46fee205eff26c4a
r.antsupov <rremon2537@gmail.com>
2025-12-19 15:25:19 +0300

    issue #15: Инициализировать фронтенд-проект на React с компонентом UsersPage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import UsersPage from "./pages/UsersPage";
+import UsersCreate from "./pages/UsersCreate";
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Users</Link>
+        <Link to="/users">users</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<UsersPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/users/create" element={<UsersCreate />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/UsersCreate.tsx
@@ -0,0 +1,9 @@
+import UsersForm from "../components/UsersForm/UsersForm";
+
+export default function UsersCreate() {
+  return (
+    <div>
+      <UsersForm />
+    </div>
+  );
+}
