39e9750fbe56dddca5523a2d78c7d603a96bbabe
Roman Antsupov <rremon2537@gmail.com>
2026-01-21 19:06:53 +0300

    issue #49: Переработать getPage backend сервиса user.service

--- backend/src/modules/user.repository.ts
+++ backend/src/modules/user.repository.ts
@@ -1,37 +1,54 @@
-import type { IUser } from "../types/user.types";
+import type { IUser, IUserRow } from "../types/user.types";
 import { pool } from "../db";
 import { userMapper } from "../mappers/userMapper";
 
 export class UserRepository {
   async getAllUsers(): Promise<IUser[]> {
-    const { rows } = await pool.query("SELECT * FROM users");
+    const { rows } = await pool.query<IUserRow>("SELECT * FROM users");
     return rows.map(userMapper);
   }
 
+  async getPage(offset: number, limit: number): Promise<IUser[]> {
+    const { rows } = await pool.query<IUserRow>(
+      "SELECT * FROM users ORDER BY id LIMIT $1 OFFSET $2",
+      [limit, offset],
+    );
+    return rows.map(userMapper);
+  }
+
+  async getCount(): Promise<number> {
+    const { rows } = await pool.query<{ count: string }>(
+      "SELECT COUNT(*) FROM users",
+    );
+    return parseInt(rows[0].count);
+  }
+
   async getUserById(id: string): Promise<IUser> {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
+    const { rows } = await pool.query<IUserRow>(
+      "SELECT * FROM users WHERE id = $1",
+      [id],
+    );
     return userMapper(rows[0]);
   }
 
   async getUserByEmail(email: string): Promise<IUser> {
-    const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+    const { rows } = await pool.query<IUserRow>(
+      "SELECT * FROM users WHERE email = $1",
+      [email],
+    );
     return userMapper(rows[0]);
   }
 
   async getUserByUsername(username: string): Promise<IUser> {
-    const { rows } = await pool.query(
+    const { rows } = await pool.query<IUserRow>(
       "SELECT * FROM users WHERE username = $1",
-      [username]
+      [username],
     );
     return userMapper(rows[0]);
   }
 
   async createUser(user: IUser): Promise<IUser> {
-    const { rows } = await pool.query(
+    const { rows } = await pool.query<IUserRow>(
       `INSERT INTO users (id, email, username, password, first_name, last_name, avatar_url, description, birthday, phone, role, is_active, email_confirmed, last_login_at, created_at, updated_at)
        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
        RETURNING *`,
@@ -51,14 +68,14 @@ export class UserRepository {
         user.emailConfirmed,
         user.lastLoginAt,
         user.createdAt,
-				user.updatedAt
-      ]
+        user.updatedAt,
+      ],
     );
     return userMapper(rows[0]);
   }
 
   async updateUser(id: string, user: IUser): Promise<IUser> {
-    const { rows } = await pool.query(
+    const { rows } = await pool.query<IUserRow>(
       `UPDATE users SET email = $1, username = $2, first_name = $3, last_name = $4, avatar_url = $5, description = $6, birthday = $7, phone = $8, role = $9, is_active = $10, email_confirmed = $11, last_login_at = $12, created_at = $13, updated_at = $14
 			WHERE id = $15 RETURNING *`,
       [
@@ -75,9 +92,9 @@ export class UserRepository {
         user.emailConfirmed,
         user.lastLoginAt,
         user.createdAt,
-				user.updatedAt,
+        user.updatedAt,
         id,
-      ]
+      ],
     );
     return userMapper(rows[0]);
   }
