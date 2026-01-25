54e07ff2898cec029e1010539301f003c7f477aa
Roman Antsupov <rremon2537@gmail.com>
2026-01-25 14:24:50 +0300

    add tests

--- backend/tests/users/users.int.test.ts
+++ backend/tests/users/users.int.test.ts
@@ -1,14 +1,10 @@
-import { beforeAll, afterEach, describe, it, expect } from "vitest";
+import { beforeAll, afterEach, beforeEach, describe, it, expect } from "vitest";
 import request from "supertest";
 import { setupTestDB } from "../setup/vitest.setup";
 import { truncateAll } from "../helpers/db";
 
 let app: any;
 let pool: any;
-
-afterEach(async () => {
-  await truncateAll();
-});
 
 beforeAll(async () => {
   await setupTestDB();
@@ -42,7 +38,40 @@ beforeAll(async () => {
   `);
 });
 
+beforeEach(async () => {
+  await pool.query(
+    `
+		INSERT INTO users (id, email, username, password, first_name, last_name, avatar_url, description, birthday, phone, role, is_active, email_confirmed, last_login_at, created_at, updated_at)
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
+	`,
+    [
+      "b6252555-eb50-4665-b476-b4ba5e3eaad3",
+      "test@test.com",
+      "testuser",
+      "hashedpassword",
+      "testfirstname",
+      "testlastname",
+      "https://example.com/avatar.jpg",
+      "Seed user",
+      "2022-01-01T00:00:00.000Z",
+      "1234567890",
+      "user",
+      true,
+      false,
+      null,
+      "2022-01-01T00:00:00.000Z",
+      "2022-01-01T00:00:00.000Z",
+    ],
+  );
+});
+
+afterEach(async () => {
+  await truncateAll();
+});
+
 describe("Users API (integration)", () => {
+  const idExample: string = "b6252555-eb50-4665-b476-b4ba5e3eaad3";
+
   it("creates user", async () => {
     const res = await request(app).post("/api/users").send({
       email: "test1@test.com",
@@ -58,4 +87,42 @@ describe("Users API (integration)", () => {
 
     expect(res.status).toBe(201);
   });
+
+  it("returns page of users", async () => {
+    const res = await request(app)
+      .get("/api/users")
+      .query({ page: 1, limit: 10 });
+
+    expect(res.status).toBe(200);
+  });
+
+  it("returns user", async () => {
+    const res = await request(app).get(`/api/users/${idExample}`);
+
+    expect(res.status).toBe(200);
+  });
+
+	it("updates user", async () => {
+		const res = await request(app)
+			.put(`/api/users/${idExample}`)
+			.send({
+				email: "test1@test.com",
+				username: "testuser1",
+				password: "hashedpassword",
+				firstName: "Test",
+				lastName: "User",
+				avatarUrl: "https://example.com/avatar.jpg",
+				description: "Seed user",
+				birthday: "2000-01-01",
+				phoneNumber: "1234567890",
+			});
+
+		expect(res.status).toBe(200);
+	});
+
+	it("deletes user", async () => {
+		const res = await request(app).delete(`/api/users/${idExample}`);
+
+		expect(res.status).toBe(200);
+	});
 });
